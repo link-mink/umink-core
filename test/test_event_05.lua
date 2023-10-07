3e8dd2f86a5d917e66a37bebf71b089f86c3ac74
Damir Franusic <damir.franusic@gmail.com>
2023-10-07 10:55:31 +0200

    feat(tests): add umlua tests for M.signal errors

--- test/test_event_05.lua
+++ test/test_event_05.lua
@@ -1,2 +1,13 @@
-local d = M.signal("TEST_EVENT_01")
-return d
+local a,r = M.signal()
+-- unknwon signal
+if r ~= 1 then
+    return "ERR"
+else
+    -- invalid type
+    a, r = M.signal(nil)
+    if r~= 7 then
+    else
+        a, r = M.signal("TEST_EVENT_01")
+        return a
+    end
+end
