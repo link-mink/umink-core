43ba0ac0cc3858083ed4fd2df682a4cd435bbcfa
Damir Franusic <damir.franusic@gmail.com>
2023-10-24 13:32:27 +0200

    feat: add OpenWRT plugin with umink ubus support

---
+++ src/services/sysagent/plugins/openwrt/plg_sysagent_openwrt.c
@@ -0,0 +1,328 @@
+/*
+ *               _____  ____ __
+ *   __ ____ _  /  _/ |/ / //_/
+ *  / // /  ' \_/ //    / ,<
+ *  \_,_/_/_/_/___/_/|_/_/|_|
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include <umink_pkg_config.h>
+#include <umink_plugin.h>
+#include <umatomic.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <string.h>
+#include <umdaemon.h>
+#include <time.h>
+#include <json_object.h>
+#include <spscq.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <libubus.h>
+#include <libubox/blob.h>
+#include <libubox/blobmsg.h>
+#include <libubox/blobmsg_json.h>
+#include <poll.h>
+#include <umlua.h>
+
+/*************/
+/* Plugin ID */
+/*************/
+static const char *PLG_ID = "plg_sysagent_openwrt.so";
+
+/**********************************************/
+/* list of command implemented by this plugin */
+/**********************************************/
+int COMMANDS[] = {
+    // end of list marker
+    -1
+};
+
+// globals
+static struct ubus_context *uctx;
+static pthread_t ubus_th;
+static struct blob_buf b;
+static umplg_mngr_t *umplgm;
+
+// list_signals policiy
+static const struct blobmsg_policy list_signals_policy[] = {};
+
+// run_signal policy order
+enum {
+    RUN_SIGNAL_ID,
+    RUN_SIGNAL_ARGS,
+    __RUN_SIGNAL_MAX
+};
+
+// run_signal policiy
+static const struct blobmsg_policy run_signal_policy[] = {
+    [RUN_SIGNAL_ID] = { .name = "id", .type = BLOBMSG_TYPE_STRING },
+    [RUN_SIGNAL_ARGS] = { .name = "args", .type = BLOBMSG_TYPE_STRING }
+
+};
+
+// match umsignal callback
+static void
+signal_match_cb(umplg_sh_t *shd, void *args)
+{
+    // skip special signals
+    if (shd->id[0] == '@') {
+        return;
+    }
+    void *json_list = blobmsg_open_table(&b, NULL);
+    blobmsg_add_string(&b, "name", shd->id);
+    // get lua env
+    struct lua_env_d **env = utarray_eltptr(shd->args, 1);
+    blobmsg_add_string(&b, "path", ((*env)->path));
+    blobmsg_add_u32 (&b, "interval", ((*env)->interval));
+    blobmsg_add_u8(&b, "auto_start", UM_ATOMIC_GET(&(*env)->active));
+    blobmsg_close_table(&b, json_list);
+}
+
+// run signal method
+static int
+run_signal(struct ubus_context *ctx,
+           struct ubus_object *obj,
+           struct ubus_request_data *req,
+           const char *method,
+           struct blob_attr *msg)
+{
+    blob_buf_init(&b, 0);
+    struct blob_attr *tb[__RUN_SIGNAL_MAX];
+
+    blobmsg_parse(run_signal_policy,
+                  __RUN_SIGNAL_MAX,
+                  tb,
+                  blob_data(msg),
+                  blob_len(msg));
+
+    if (tb[RUN_SIGNAL_ID] != NULL) {
+        // id and args
+        char *id = strdup(blobmsg_get_string(tb[RUN_SIGNAL_ID]));
+        char *args = NULL;
+        // check args
+        if (tb[RUN_SIGNAL_ARGS] != NULL) {
+            args = strdup(blobmsg_get_string(tb[RUN_SIGNAL_ARGS]));
+
+        } else {
+            args = strdup("");
+        }
+
+        // output buffer
+        char *buff = NULL;
+        size_t b_sz = 0;
+
+        // input data
+        umplg_data_std_t e_d = { .items = NULL };
+        umplg_data_std_items_t items = { .table = NULL };
+        umplg_data_std_item_t item = { .name = "", .value = args };
+        umplg_data_std_item_t auth_item = { .name = "", .value = "" };
+        // init std data
+        umplg_stdd_init(&e_d);
+        umplg_stdd_item_add(&items, &item);
+        umplg_stdd_item_add(&items, &auth_item);
+        umplg_stdd_items_add(&e_d, &items);
+
+        // run signal (set)
+        int r = umplg_proc_signal(umplgm, id,  &e_d, &buff, &b_sz, 0, NULL);
+
+        switch (r) {
+            case UMPLG_RES_SUCCESS:
+                if (buff && !blobmsg_add_json_from_string(&b, buff)) {
+                    blobmsg_add_string(&b, "result", buff);
+                }
+                break;
+
+            case UMPLG_RES_AUTH_ERROR:
+                blobmsg_add_string(&b, "result", "authentication error");
+                break;
+
+            case UMPLG_RES_UNKNOWN_SIGNAL:
+                blobmsg_add_string(&b, "result", "unknown signal");
+                break;
+
+            default:
+                blobmsg_add_string(&b, "result", "unknown error");
+                break;
+        }
+        HASH_CLEAR(hh, items.table);
+        umplg_stdd_free(&e_d);
+        free(buff);
+        free(id);
+        free(args);
+
+    // missing args
+    } else {
+        blobmsg_add_string(&b, "result", "missing arguments");
+    }
+
+    return ubus_send_reply(ctx, req, b.head);
+}
+
+// list signals method
+static int
+list_signals(struct ubus_context *ctx,
+             struct ubus_object *obj,
+             struct ubus_request_data *req,
+             const char *method,
+             struct blob_attr *msg)
+{
+
+    blob_buf_init(&b, 0);
+    void *json_uri = blobmsg_open_array(&b, "signals");
+
+    // init target/function specific lua modules
+    umplg_match_signal(umplgm, "*", &signal_match_cb, NULL);
+
+    blobmsg_close_array(&b, json_uri);
+
+    return ubus_send_reply(ctx, req, b.head);
+}
+
+// ubus object methods
+static const struct ubus_method umink_methods[] = {
+    UBUS_METHOD("list_signals", &list_signals, list_signals_policy),
+    UBUS_METHOD("run_signal", &run_signal, run_signal_policy),
+};
+
+// ubus object type
+static struct ubus_object_type umink_obj_type =
+    UBUS_OBJECT_TYPE("signals", umink_methods);
+
+// umink ubus object
+static struct ubus_object umink_object = {
+    .name = "umink",
+    .type = &umink_obj_type,
+    .methods = umink_methods,
+    .n_methods = ARRAY_SIZE(umink_methods),
+
+};
+
+// ubus thread
+void *
+thread_ubus(void *args)
+{
+    // setup ubus socket polling
+    struct pollfd pfd = { .fd = uctx->sock.fd, .events = POLLIN };
+    umd_log(UMD, UMD_LLT_INFO, "plg_openwrt: [ubus thread starting");
+
+    while (!umd_is_terminating()) {
+        // poll ubus socket
+        int r = poll(&pfd, 1, 1000);
+
+        // critical error
+        if (r == -1) {
+            umd_log(UMD,
+                    UMD_LLT_ERROR,
+                    "plg_openwrt: [ubus socket error (%s)]",
+                    strerror(errno));
+            break;
+
+        // socket inactive
+        } else if (pfd.revents == 0) {
+            continue;
+
+        // data or error pending
+        } else if (pfd.revents & POLLIN) {
+            ubus_handle_event(uctx);
+
+        // socket error POLLERR/POLLHUP
+        } else {
+            umd_log(UMD, UMD_LLT_ERROR, "plg_openwrt: [ubus socket error]");
+            break;
+        }
+    }
+
+    umd_log(UMD, UMD_LLT_INFO, "plg_openwrt: [ubus thread terminating");
+    return NULL;
+}
+
+/****************/
+/* init handler */
+/****************/
+int
+init(umplg_mngr_t *pm, umplgd_t *pd)
+{
+    umplgm = pm;
+    signal(SIGPIPE, SIG_IGN);
+    uctx = ubus_connect(NULL);
+    if (uctx == NULL) {
+        umd_log(UMD, UMD_LLT_ERROR, "plg_openwrt: [cannot connect to ubusd]");
+        return 1;
+    }
+    int r = ubus_add_object(uctx, &umink_object);
+    if (r) {
+        umd_log(UMD,
+                UMD_LLT_ERROR,
+                "plg_openwrt: [cannot add umink object, %s]",
+                ubus_strerror(r));
+        ubus_free(uctx);
+        uctx = NULL;
+        return 1;
+    }
+    pthread_create(&ubus_th, NULL, &thread_ubus, NULL);
+
+    return 0;
+}
+
+/*********************/
+/* terminate handler */
+/*********************/
+static void
+term_phase_0(umplg_mngr_t *pm, umplgd_t *pd)
+{
+    if (uctx != NULL) {
+        pthread_join(ubus_th, NULL);
+    }
+}
+
+static void
+term_phase_1(umplg_mngr_t *pm, umplgd_t *pd)
+{
+    if (uctx != NULL) {
+        ubus_free(uctx);
+    }
+    blob_buf_free(&b);
+}
+
+int
+terminate(umplg_mngr_t *pm, umplgd_t *pd, int phase)
+{
+    switch (phase) {
+    case 0:
+        term_phase_0(pm, pd);
+        break;
+    case 1:
+        term_phase_1(pm, pd);
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
+
+
+/*************************/
+/* local command handler */
+/*************************/
+// GCOVR_EXCL_START
+int
+run_local(umplg_mngr_t *pm, umplgd_t *pd, int cmd_id, umplg_idata_t *data)
+{
+    // not used
+    return 0;
+}
+
+/*******************/
+/* command handler */
+/*******************/
+int
+run(umplg_mngr_t *pm, umplgd_t *pd, int cmd_id, umplg_idata_t *data)
+{
+    // not used
+    return 0;
+}
+// GCOVR_EXCL_STOP
---
+++ test/check_openwrt.c
@@ -0,0 +1,386 @@
+/*
+ *               _____  ____ __
+ *   __ ____ _  /  _/ |/ / //_/
+ *  / // /  ' \_/ //    / ,<
+ *  \_,_/_/_/_/___/_/|_/_/|_|
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#include <umink_pkg_config.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <string.h>
+#include <stdio.h>
+#include <signal.h>
+#include <umdaemon.h>
+#include <json_tokener.h>
+#include <umink_plugin.h>
+#include <unistd.h>
+#include <math.h>
+#include <cmocka_tests.h>
+#include <umlua.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <libubus.h>
+#include <libubox/blob.h>
+#include <libubox/blobmsg.h>
+#include <libubox/blobmsg_json.h>
+
+// dummy struct for state passing
+typedef struct {
+    umdaemon_t *umd;
+    umplg_mngr_t *m;
+} test_t;
+
+// fwd declarations
+void umlua_shutdown();
+int umlua_init(umplg_mngr_t *pm);
+void umlua_start(umplg_mngr_t *pm);
+static int run_init(void **state);
+static int run_dtor(void **state);
+
+// globals
+static char plg_cfg_fname[128];
+static struct ubus_context *ctx;
+static struct blob_buf b;
+
+static void
+load_cfg(umplg_mngr_t *m)
+{
+    // load plugins configuration
+    char *fname = plg_cfg_fname;
+    FILE *f = fopen(fname, "r");
+    assert_non_null(f);
+    if (fseek(f, 0, SEEK_END) < 0) {
+        fclose(f);
+        fail();
+    }
+    int32_t fsz = ftell(f);
+    if (fsz <= 0) {
+        fclose(f);
+        fail();
+    }
+    rewind(f);
+    char *b = calloc(fsz + 1, 1);
+    fread(b, fsz, 1, f);
+    fclose(f);
+
+    // process plugins configurataion
+    m->cfg = json_tokener_parse(b);
+    if (m->cfg == NULL) {
+        fail_msg("ERROR: Invalid plugins configuration file");
+    }
+    free(b);
+}
+
+static int
+run_init(void **state)
+{
+    test_t *data = malloc(sizeof(test_t));
+    data->umd = umd_create("test_id", "test_type");
+    data->m = umplg_new_mngr();
+    data->umd->perf = umc_new_ctx();
+
+    // load dummy cfg
+    load_cfg(data->m);
+
+    // init lua core
+    umlua_init(data->m);
+
+    // load MQTT plugin
+    umplgd_t *p = umplg_load(data->m, ".libs/check_plg_sysagent_openwrt.so");
+    assert_non_null(p);
+
+    // start lua envs
+    umlua_start(data->m);
+
+    // connect to ubus
+    ctx = ubus_connect(NULL);
+    assert_non_null(ctx);
+    ubus_add_uloop(ctx);
+
+    *state = data;
+
+    return 0;
+}
+
+static int
+run_dtor(void **state)
+{
+    test_t *data = *state;
+    // stop
+    umd_signal_handler(SIGTERM);
+    // free
+    umlua_shutdown();
+    json_object_put(data->m->cfg);
+    umplg_free_mngr(data->m);
+    umc_free_ctx(data->umd->perf);
+    umd_destroy(data->umd);
+    free(data);
+    ubus_free(ctx);
+    blob_buf_free(&b);
+    return 0;
+}
+
+static void
+list_signals_cb(struct ubus_request *req, int type, struct blob_attr *msg)
+{
+    // ubus output
+    char *str = blobmsg_format_json(msg, true);
+    assert_non_null(str);
+
+    // add result
+    char **out_str = (char **)req->priv;
+    *out_str = str;
+}
+
+static void
+run_signal_w_static_output_cb(struct ubus_request *req,
+                              int type,
+                              struct blob_attr *msg)
+{
+    // ubus output
+    char *str = blobmsg_format_json(msg, true);
+    assert_non_null(str);
+
+    // add result
+    char **out_str = (char **)req->priv;
+    *out_str = str;
+}
+
+static void
+run_missing_signal_cb(struct ubus_request *req, int type, struct blob_attr *msg)
+{
+    // ubus output
+    char *str = blobmsg_format_json(msg, true);
+    assert_non_null(str);
+
+    // add result
+    char **out_str = (char **)req->priv;
+    *out_str = str;
+}
+
+static void
+run_signal_w_args_return_named_arg_cb(struct ubus_request *req,
+                                      int type,
+                                      struct blob_attr *msg)
+{
+    // ubus output
+    char *str = blobmsg_format_json(msg, true);
+    assert_non_null(str);
+
+    // add result
+    char **out_str = (char **)req->priv;
+    *out_str = str;
+}
+
+static void
+run_signal_w_id_missing_cb(struct ubus_request *req,
+                           int type,
+                           struct blob_attr *msg)
+{
+    // ubus output
+    char *str = blobmsg_format_json(msg, true);
+    assert_non_null(str);
+
+    // add result
+    char **out_str = (char **)req->priv;
+    *out_str = str;
+}
+
+static void
+call_list_signals(void **state)
+{
+    uint32_t id;
+
+    // look for umink object
+    int r = ubus_lookup_id(ctx, "umink", &id);
+    assert_int_equal(r, 0);
+
+    blob_buf_init(&b, 0);
+    blobmsg_add_u32(&b, "id", id);
+    char *out_str = NULL;
+
+    r = ubus_invoke(ctx,
+                    id,
+                    "list_signals",
+                    b.head,
+                    list_signals_cb,
+                    &out_str,
+                    2000);
+    assert_int_equal(r, 0);
+    assert_non_null(out_str);
+
+    // expected result
+    FILE *f = fopen("./test/ubus_output_01.json", "r");
+    assert_non_null(f);
+    if (fseek(f, 0, SEEK_END) < 0) {
+        fclose(f);
+        fail();
+    }
+    int32_t fsz = ftell(f);
+    if (fsz <= 0) {
+        fclose(f);
+        fail();
+    }
+    rewind(f);
+    char *buff = calloc(fsz, 1);
+    fread(buff, fsz, 1, f);
+    fclose(f);
+
+    // get result from ubus call
+    assert_string_equal(out_str, buff);
+
+    // free
+    free(out_str);
+    free(buff);
+}
+
+static void
+call_run_signal_w_static_output(void **state)
+{
+    uint32_t id;
+
+    // look for umink object
+    int r = ubus_lookup_id(ctx, "umink", &id);
+    assert_int_equal(r, 0);
+
+    blob_buf_init(&b, 0);
+    blobmsg_add_u32(&b, "id", id);
+    blobmsg_add_json_from_string(&b, "{\"id\": \"TEST_EVENT_01\"}");
+    char *out_str = NULL;
+
+    r = ubus_invoke(ctx,
+                    id,
+                    "run_signal",
+                    b.head,
+                    run_signal_w_static_output_cb,
+                    &out_str,
+                    2000);
+    assert_int_equal(r, 0);
+    assert_non_null(out_str);
+
+    // get result from ubus call
+    assert_string_equal(out_str, "{\"result\":\"test_data\"}");
+
+    // free
+    free(out_str);
+}
+
+static void
+call_run_missing_signal(void **state)
+{
+    uint32_t id;
+
+    // look for umink object
+    int r = ubus_lookup_id(ctx, "umink", &id);
+    assert_int_equal(r, 0);
+
+    blob_buf_init(&b, 0);
+    blobmsg_add_u32(&b, "id", id);
+    blobmsg_add_json_from_string(&b, "{\"id\": \"TEST_EVENT_XX\"}");
+    char *out_str = NULL;
+
+    r = ubus_invoke(ctx,
+                    id,
+                    "run_signal",
+                    b.head,
+                    run_missing_signal_cb,
+                    &out_str,
+                    2000);
+    assert_int_equal(r, 0);
+    assert_non_null(out_str);
+
+    // get result from ubus call
+    assert_string_equal(out_str, "{\"result\":\"unknown signal\"}");
+
+    // free
+    free(out_str);
+}
+
+static void
+call_run_signal_w_args_return_named_arg(void **state)
+{
+    uint32_t id;
+
+    // look for umink object
+    int r = ubus_lookup_id(ctx, "umink", &id);
+    assert_int_equal(r, 0);
+
+    blob_buf_init(&b, 0);
+    blobmsg_add_u32(&b, "id", id);
+    blobmsg_add_json_from_string(
+        &b,
+        "{\"id\": \"TEST_EVENT_03_UBUS\", "
+        "\"args\": \"{\\\"test_key\\\": \\\"test_arg_data\\\"}\"}");
+    char *out_str = NULL;
+
+    r = ubus_invoke(ctx,
+                    id,
+                    "run_signal",
+                    b.head,
+                    run_signal_w_args_return_named_arg_cb,
+                    &out_str,
+                    2000);
+    assert_int_equal(r, 0);
+    assert_non_null(out_str);
+
+    // get result from ubus call
+    assert_string_equal(out_str, "{\"test_key\":\"test_arg_data\"}");
+
+    // free
+    free(out_str);
+}
+
+static void
+call_run_signal_w_id_missing(void **state)
+{
+    uint32_t id;
+
+    // look for umink object
+    int r = ubus_lookup_id(ctx, "umink", &id);
+    assert_int_equal(r, 0);
+
+    blob_buf_init(&b, 0);
+    blobmsg_add_u32(&b, "id", id);
+    char *out_str = NULL;
+
+    r = ubus_invoke(ctx,
+                    id,
+                    "run_signal",
+                    b.head,
+                    run_signal_w_id_missing_cb,
+                    &out_str,
+                    2000);
+    assert_int_equal(r, 0);
+    assert_non_null(out_str);
+
+    // get result from ubus call
+    assert_string_equal(out_str, "{\"result\":\"missing arguments\"}");
+
+    // free
+    free(out_str);
+}
+
+int
+main(int argc, char **argv)
+{
+    strcpy(plg_cfg_fname, "test/plg_cfg.json");
+
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(call_list_signals),
+        cmocka_unit_test(call_run_signal_w_static_output),
+        cmocka_unit_test(call_run_missing_signal),
+        cmocka_unit_test(call_run_signal_w_id_missing),
+        cmocka_unit_test(call_run_signal_w_args_return_named_arg),
+    };
+
+    int r = cmocka_run_group_tests(tests, run_init, run_dtor);
+    return r;
+
+}
+
